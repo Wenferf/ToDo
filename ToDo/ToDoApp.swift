f5f578ab7b8a7265d4096585d1ada6bacbb6b6f8
Tucker Gillbrand <tucker@gillbrand.com>
2021-07-28 08:41:16 -0700

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Tucker(School) on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
